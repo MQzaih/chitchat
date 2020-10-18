4f8df895791fd2c1b55f925fc095788f341afccd
MQzaih <masaqzaih@gmail.com>
2020-10-18 22:24:34 +0300

    Initial Commit

---
+++ chitchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chitchat
+//
+//  Created by Asal 2 on 18/10/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
