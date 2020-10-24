8f37cf3bca605c14eab9257a38a5c8f7af639487
MQzaih <masaqzaih@gmail.com>
2020-10-24 17:00:09 +0300

    first commit

---
+++ chitchat/ChatMessages.swift
@@ -0,0 +1,9 @@
+//
+//  ChatMessages.swift
+//  chitchat
+//
+//  Created by Asal 2 on 18/10/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+import Foundation
---
+++ chitchat/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  chitchat
+//
+//  Created by Asal 2 on 18/10/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
